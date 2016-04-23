900f2d2635eb2da5f91aa3b341cd1242d6debe0f
Susan Hert <susanh@labkey.com>
2016-04-23 02:45:39 +0000

    move tasks for creating module.xml and .module files into Module plugin and add various module properties

--- src/main/groovy/org/labkey/gradle/plugin/Module.groovy
+++ src/main/groovy/org/labkey/gradle/plugin/Module.groovy
@@ -1,7 +1,13 @@
 package org.labkey.gradle.plugin
 
-import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.bundling.Jar
+
+import java.text.SimpleDateFormat
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 
 /**
@@ -18,9 +24,10 @@ class Module extends LabKey
     //   ext.skipBuild = true
     def String _skipBuildFile = "skipBuild.txt"
     def String _modulePropertiesFile = "module.properties"
-    def String _explodedModuleDir = "explodedModule"
+    private static final String ENLISTMENT_PROPERTIES = "enlistment.properties"
     def Properties _moduleProperties;
     def Project _project;
+    def Pattern PROPERTY_PATTERN = Pattern.compile("@@([^@]+)@@")
 
     @Override
     void apply(Project project)
@@ -49,30 +56,46 @@ class Module extends LabKey
             }
             return indicators.isEmpty()
         })
-        readModuleProperties()
-        setVersion()
+        setModuleProperties()
+        addTasks()
         addDependencies()
     }
 
-
-
-    private void readModuleProperties()
+    private void setVcsProperties()
     {
-        File propertiesFile = _project.file(_modulePropertiesFile);
-        if (propertiesFile.exists())
-        {
-            _moduleProperties = new Properties();
-            FileInputStream is = new FileInputStream(_project.file(_modulePropertiesFile))
-            _moduleProperties.load(is)
-        }
-        setVersion();
+        _moduleProperties.setProperty("VcsURL", "???") // TODO
+        _moduleProperties.setProperty("VcsRevision", "???")
+        _moduleProperties.setProperty("BuildNumber", "???") // TODO where does this come from in TeamCity?
     }
 
-    private void setVersion()
+    private setEnlistmentId()
     {
-        if (_moduleProperties != null && _moduleProperties.containsKey("Version"))
-            _project.version = _moduleProperties.get("Version");
-        _project.version = _project.rootProject.version;
+        File enlistmentFile = new File(_project.getRootProject().getProjectDir(), ENLISTMENT_PROPERTIES)
+        Properties enlistmentProperties = new Properties()
+        if (!enlistmentFile.exists())
+        {
+            UUID id = UUID.randomUUID()
+            enlistmentProperties.setProperty("enlistment.id", id.toString())
+            enlistmentProperties.store(new FileWriter(enlistmentFile), SimpleDateFormat.getDateTimeInstance().format(new Date()))
+        }
+        else
+        {
+            readProperties(enlistmentFile, enlistmentProperties)
+        }
+        _moduleProperties.setProperty("EnlistmentId", enlistmentProperties.getProperty("enlistment.id"))
+    }
+
+    private void setBuildInfoProperties()
+    {
+        _moduleProperties.setProperty("BuildType", _project.labkey.deployMode.toString())
+        _moduleProperties.setProperty("BuildUser", System.getProperty("user.name"))
+        _moduleProperties.setProperty("BuildOS", System.getProperty("os.name"))
+        _moduleProperties.setProperty("BuildTime", SimpleDateFormat.getDateTimeInstance().format(new Date()))
+        _moduleProperties.setProperty("BuildPath", _project.buildDir.getAbsolutePath() )
+        _moduleProperties.setProperty("SourcePath", _project.projectDir.getAbsolutePath() )
+        _moduleProperties.setProperty("ResourcePath", "") // TODO  _project.getResources().... ???
+        _moduleProperties.setProperty("ConsolidateScripts", "")
+        _moduleProperties.setProperty("ManageVersion", "")
     }
 
     private void addDependencies()
@@ -83,14 +106,106 @@ class Module extends LabKey
                     compile _project.project(":server:internal")
                     compile _project.project(":remoteapi:java")
                     compile _project.fileTree(dir: "${_project.labkey.explodedModuleDir}/lib", include: '*.jar')
-//                    // TODO this adds in all of the api jar files, mimicking what is done in ant.  We should
-//                    // replace this in favor of specific versions.
-//                    compile _project.fileTree(dir: "${_project.labkey.modulesApiDir}", include: '*.jar')
                 }
         _project.tasks.compileJava.dependsOn('schemasJar')
         _project.tasks.compileJava.dependsOn('apiJar')
         _project.tasks.jsp2Java.dependsOn('apiJar')
     }
 
+    private static void readProperties(File propertiesFile, Properties properties)
+    {
+        if (propertiesFile.exists())
+        {
+            FileInputStream is;
+            try
+            {
+                is = new FileInputStream(propertiesFile)
+                properties.load(is)
+            }
+            finally
+            {
+                if (is != null)
+                    is.close()
+            }
+
+        }
+    }
+
+    private void setModuleProperties()
+    {
+        File propertiesFile = _project.file(_modulePropertiesFile)
+        _moduleProperties = new Properties()
+        readProperties(propertiesFile, _moduleProperties)
+
+        _moduleProperties.setProperty("Version", _project.version.toString())
+        setBuildInfoProperties()
+        setVcsProperties()
+        setEnlistmentId()
+    }
+
+    private void addTasks()
+    {
+
+        def Task modulesXmlTask = _project.task('modulesXml',
+                group: "module",
+                type: Copy,
+                description: "create the module.xml file using module.properties",
+                {
+                    from _project.project(":server").projectDir
+                    include 'module.template.xml'
+                    rename {"module.xml"}
+                    filter( { String line ->
+                        def Matcher matcher = PROPERTY_PATTERN.matcher(line);
+                        def String newLine = line;
+                        while (matcher.find())
+                        {
+                            if (_moduleProperties.containsKey(matcher.group(1)))
+                            {
+                                newLine = newLine.replace(matcher.group(), (String) _moduleProperties.get(matcher.group(1)))
+                            }
+                            else
+                            {
+                                newLine = newLine.replace(matcher.group(), "")
+                            }
+                        }
+                        return newLine;
+
+                    })
+                    destinationDir = new File((String) _project.sourceSets.spring.output.resourcesDir)
+                }
+        )
+        modulesXmlTask.outputs.upToDateWhen(
+                {
+                    Task task ->
+                        File moduleXmlFile = new File((String) _project.sourceSets.spring.output.resourcesDir, "/module.xml")
+                        if (!moduleXmlFile.exists())
+                            return false
+                        else
+                        {
+                            if (_project.file(_modulePropertiesFile).lastModified() > moduleXmlFile.lastModified() ||
+                                _project.project(":server").file('module.template.xml').lastModified() > moduleXmlFile.lastModified())
+                                return false
+                        }
+                        return true
+                }
+        )
+
+        def Task moduleFile = _project.task("moduleFile",
+                group: "module",
+                type: Jar,
+                description: "create ",
+                {
+                    from _project.labkey.explodedModuleDir
+                    exclude '**/*.uptodate'
+                    exclude "META-INF/${_project.name}/**"
+                    exclude 'gwt-unitCache/**'
+                    //baseName "${_project.name}"
+                    //extension 'module'
+                    archiveName "${_project.name}.module" // TODO remove this in favor of a versioned jar file when other items have change
+                    destinationDir = new File((String) _project.labkey.stagingModulesDir)
+                }
+        )
+        moduleFile.dependsOn(modulesXmlTask, _project.tasks.assemble) // TODO is this the right dependency?
+    }
 }
 
--- src/main/groovy/org/labkey/gradle/plugin/SpringConfig.groovy
+++ src/main/groovy/org/labkey/gradle/plugin/SpringConfig.groovy
@@ -15,11 +15,6 @@ class SpringConfig extends LabKey
 {
     def Project _project;
     def String _configDir;
-    def Map<String, String> _tokens = [
-        'VcsRevision': "Unknown",
-        'VcsURL': "URL",
-    ]
-    def Pattern PROPERTY_PATTERN = Pattern.compile("@@([^@]+)@@")
 
     @Override
     void apply(Project project)
@@ -28,20 +23,7 @@ class SpringConfig extends LabKey
         _configDir = "${_project.labkey.explodedModuleDir}/config"
         project.apply plugin: 'java-base'
 
-        addConfiguration()
-        addDependencies()
         addSourceSet()
-        addTasks()
-    }
-
-    private void addConfiguration()
-    {
-
-    }
-
-    private void addDependencies()
-    {
-
     }
 
     private void addSourceSet()
@@ -55,50 +37,5 @@ class SpringConfig extends LabKey
                         output.resourcesDir = _configDir
                     }
                 }
-    }
-
-    private void addTasks()
-    {
-        def Task modulesXmlTask = _project.task('modulesXml',
-                group: "module",
-                type: Copy,
-                description: "create the module.xml file using module.properties",
-                        {
-                            println("copy from ${_project.project(":server").projectDir} to ${_configDir}")
-                            from _project.project(":server").projectDir
-                            include '*.template.xml'
-                            rename {"module.xml"}
-                            filter( { line ->
-                                def Matcher matcher = PROPERTY_PATTERN.matcher(line);
-                                def String newLine = line;
-                                while (matcher.find())
-                                {
-                                    if (_tokens.containsKey(matcher.group(1)))
-                                    {
-                                        newLine = newLine.replace(matcher.group(), _tokens.get(matcher.group(1)))
-                                    }
-                                }
-                                return newLine;
-
-                            })
-                            destinationDir = new File(_configDir)
-                        }
-        )
-
-        def Task moduleFile = _project.task("moduleFile",
-                group: "module",
-                type: Jar,
-                description: "create ", {
-            from _project.labkey.explodedModuleDir
-            exclude '**/*.uptodate'
-            exclude "META-INF/${_project.name}/**"
-            exclude 'gwt-unitCache/**'
-            //baseName "${_project.name}"
-            //extension 'module'
-            archiveName "${_project.name}.module" // TODO remove this in favor of a versioned jar file when other items have change
-            destinationDir = new File(_project.labkey.stagingModulesDir)
-        }
-        )
-        moduleFile.dependsOn(modulesXmlTask, _project.tasks.assemble)
     }
 }
