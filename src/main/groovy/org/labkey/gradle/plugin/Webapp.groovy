1d4c287eecdb56805e2fa1af13b86b93c052f59c
Susan Hert <susanh@labkey.com>
2016-07-29 15:36:15 +0000

    update comment - no longer a question here

--- src/main/groovy/org/labkey/gradle/plugin/Webapp.groovy
+++ src/main/groovy/org/labkey/gradle/plugin/Webapp.groovy
@@ -24,7 +24,7 @@ class Webapp implements Plugin<Project>
                             srcDirs = ['webapp']
                             // The spring configuration files are copied by the SpringConfig plugin
                             exclude "WEB-INF/${project.name}/**"
-                            // when in dev mode, the webapp files will be picked up from their original locations (???)
+                            // when in dev mode, the webapp files will be picked up from their original locations
                             if (project.labkey.deployMode == LabKey.DeployMode.dev)
                                 include 'share/**', 'WEB-INF/**'
 
