0e5dff7ad637a9d000937d3de501f0dfbdfdb0d9
Susan Hert <susanh@labkey.com>
2016-08-02 03:06:35 +0000

    add TODO comment

--- src/main/groovy/org/labkey/gradle/plugin/Jsp.groovy
+++ src/main/groovy/org/labkey/gradle/plugin/Jsp.groovy
@@ -55,7 +55,15 @@ class Jsp extends LabKey
     {
         project.dependencies
                 {
-                    jspCompile  'org.apache.tomcat:jasper',
+                    // TODO there may be a need to add other items to the classpath here to get rid of these warnings:
+//                    The XML schema [web-app_3_1.xsd] could not be found. This is very likely to break XML validation if XML validation is enabled.
+//                    The XML schema [web-fragment_3_1.xsd] could not be found. This is very likely to break XML validation if XML validation is enabled.
+//                    The XML schema [web-common_3_1.xsd] could not be found. This is very likely to break XML validation if XML validation is enabled.
+//                    The XML schema [javaee_7.xsd] could not be found. This is very likely to break XML validation if XML validation is enabled.
+//                    The XML schema [jsp_2_3.xsd] could not be found. This is very likely to break XML validation if XML validation is enabled.
+//                    The XML schema [javaee_web_services_1_4.xsd] could not be found. This is very likely to break XML validation if XML validation is enabled.
+//                    The XML schema [javaee_web_services_client_1_4.xsd] could not be found. This is very likely to break XML validation if XML validation is enabled.
+                        jspCompile  'org.apache.tomcat:jasper',
                         'org.apache.tomcat:jsp-api',
                         'javax.servlet:servlet-api:3.1',
                         'org.apache.tomcat:tomcat-juli'
