ea6a19538d4d72dc1af0e9e3c1300112868f06be
Susan Hert <susanh@labkey.com>
2016-08-09 21:29:38 +0000

    fix typo in log4j setup

--- customPlugins/src/main/groovy/org/labkey/gradle/task/ConfigureLog4J.groovy
+++ customPlugins/src/main/groovy/org/labkey/gradle/task/ConfigureLog4J.groovy
@@ -2,10 +2,9 @@ package org.labkey.gradle.task
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 import org.labkey.gradle.plugin.LabKey
-
 /**
  * Created by susanh on 8/2/16.
  */
@@ -14,15 +13,19 @@ class ConfigureLog4J extends DefaultTask
     @InputFile
     File log4jXML = new File("${project.serverDeploy.rootWebappsDir}/log4j.xml")
 
-    @OutputDirectory
-    File outputDir = new File(project.labkey.webappClassesDir)
+
+    File outputDir = new File("${project.labkey.webappClassesDir}")
+
+    @OutputFile
+    File outputFile = new File(outputDir, "log4j.xml")
+
     @TaskAction
     def compress()
     {
         def String consoleAppender = "" // this is the setting for production mode
         if (project.labkey.deployMode == LabKey.DeployMode.dev)
         {
-            consoleAppender = '<appender-ref ref="CONSOLE">"'
+            consoleAppender = '<appender-ref ref="CONSOLE"/>'
         }
         ant.copy(
                 todir: outputDir,
