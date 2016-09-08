54e2ccc08c4f8e704e723ee138d8f4746e0fe826
Susan Hert <susanh@labkey.com>
2016-09-08 23:30:51 +0000

    move methods and constants for selecting modules to groovy utility class

---
+++ customPlugins/src/main/groovy/org/labkey/gradle/util/BuildUtils.groovy
@@ -0,0 +1,70 @@
+package org.labkey.gradle.util
+
+import org.gradle.api.initialization.Settings
+
+/**
+ * Static utility methods and constants for use in the build and settings scripts.
+ */
+class BuildUtils
+{
+    public static final String SERVER_MODULE_DIR = "server/modules"
+
+    public static final List<String>  baseModules = ["server:bootstrap",
+                                                     "server:api",
+                                                     "schemas",
+                                                     "server:internal",
+                                                     'remoteapi:java'
+    ]
+
+
+    // a set of directory paths in which to look for module directories
+    public static final List<String> SERVER_MODULE_DIRS = [SERVER_MODULE_DIR,
+                                                           "server/customModules",
+                                                           "server/optionalModules"]
+
+    public static final List<String> EXTERNAL_MODULE_DIRS = ["externalModules",
+                                                             "externalModules/scharp",
+                                                             "externalModules/labModules",
+                                                             "externalModules/onprcEHRModules",
+                                                             "externalModules/snprcEHRModules",
+                                                             "externalModules/DISCVR"]
+
+    /**
+     * This includes modules that are required for any LabKey server build (e.g., bootstrap, api, internal)
+     * @param settings the settings
+     */
+    public static void includeBaseModules(Settings settings)
+    {
+        settings.include baseModules.toArray(new String[0])
+    }
+
+    /**
+     * Can be used in a gradle settings file to include the projects in a particular directory.
+     * @param rootDir - the root directory for the gradle build (project.rootDir)
+     * @param moduleDirs - the list of directories that are parents of module directories to be included
+     * @param excludedModules - a list of directory names that are to be excluded from the build configuration (e.g., movies)
+     */
+    public static void includeModules(Settings settings, File rootDir, List<String> moduleDirs, List<String> excludedModules)
+    {
+        // find the directories in each of the moduleDirs that meet our selection criteria
+        moduleDirs.each { String path ->
+            File directory = new File(rootDir, path);
+            if (directory.exists())
+            {
+                String relativePath = directory.absolutePath - rootDir.absolutePath
+                String prefix = relativePath.replaceAll("[\\\\\\/]", ":")
+                settings.include directory.listFiles().findAll { File f ->
+                    // exclude non-directories, explicitly excluded names, and directories beginning with a .
+                    f.isDirectory() && !excludedModules.contains(f.getName()) && !(f =~ ".*/\\..*") && !(f =~ "^\\..*")
+                }.collect {
+                    (String) "${prefix}:${it.getName()}"
+                }.toArray(new String[0])
+            }
+        }
+        if (moduleDirs.contains(SERVER_MODULE_DIR) && !excludedModules.contains("enginesrc"))
+        {
+            settings.include 'server:modules:flow:enginesrc'
+            // this is included separately since there's no good way to detect it programmatically
+        }
+    }
+}
