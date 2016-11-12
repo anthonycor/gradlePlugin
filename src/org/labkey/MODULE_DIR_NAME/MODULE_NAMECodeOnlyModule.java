8f94cae7ab54548c12c2cea202da5bee76c202a2
Adam Rauch <adamr@labkey.com>
2016-11-12 08:02:50 +0000

    Add option to create code-only modules via create module task
    Update copyright dates and script versions on template files

--- src/org/labkey/MODULE_DIR_NAME/MODULE_NAMECodeOnlyModule.java
+++ src/org/labkey/MODULE_DIR_NAME/MODULE_NAMECodeOnlyModule.java
@@ -19,15 +19,14 @@ package org.labkey.@@MODULE_LOWERCASE_NAME@@;
 import org.jetbrains.annotations.NotNull;
 import org.labkey.api.data.Container;
 import org.labkey.api.data.ContainerManager;
-import org.labkey.api.module.DefaultModule;
+import org.labkey.api.module.CodeOnlyModule;
 import org.labkey.api.module.ModuleContext;
 import org.labkey.api.view.WebPartFactory;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Set;
 
-public class @@MODULE_NAME@@Module extends DefaultModule
+public class @@MODULE_NAME@@Module extends CodeOnlyModule
 {
     public static final String NAME = "@@MODULE_NAME@@";
 
@@ -35,18 +34,6 @@ public class @@MODULE_NAME@@Module extends DefaultModule
     public String getName()
     {
         return NAME;
-    }
-
-    @Override
-    public double getVersion()
-    {
-        return 16.21;
-    }
-
-    @Override
-    public boolean hasScripts()
-    {
-        return true;
     }
 
     @Override
@@ -74,12 +61,5 @@ public class @@MODULE_NAME@@Module extends DefaultModule
     public Collection<String> getSummary(Container c)
     {
         return Collections.emptyList();
-    }
-
-    @Override
-    @NotNull
-    public Set<String> getSchemaNames()
-    {
-        return Collections.singleton(@@MODULE_NAME@@Schema.NAME);
     }
 }
