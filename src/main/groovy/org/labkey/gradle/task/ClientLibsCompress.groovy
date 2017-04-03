020dd0d761728aadc96fd8cc06c1f469255d3c49
Susan Hert <susanh@labkey.com>
2017-04-02 19:12:31 -0700

    remove use of yuiCompressor jar via command line in favor of using the Compressor classes

--- src/main/groovy/org/labkey/gradle/task/ClientLibsCompress.groovy
+++ src/main/groovy/org/labkey/gradle/task/ClientLibsCompress.groovy
@@ -1,5 +1,7 @@
 package org.labkey.gradle.task
 
+import com.yahoo.platform.yui.compressor.CssCompressor
+import com.yahoo.platform.yui.compressor.JavaScriptCompressor
 import org.apache.commons.io.FileUtils
 import org.apache.commons.io.IOUtils
 import org.gradle.api.DefaultTask
@@ -18,13 +20,9 @@ import java.nio.charset.StandardCharsets
 
 class ClientLibsCompress extends DefaultTask
 {
-
     public static final String LIB_XML_EXTENSION = ".lib.xml"
 
-    File workingDir = project.clientLibs.workingDir
-
-    // TODO get rid of this reference
-    String yuiCompressorJar
+    File workingDir = new File((String) project.labkey.explodedModuleWebDir)
 
     // TODO get rid of this
     @OutputFile
@@ -32,7 +30,6 @@ class ClientLibsCompress extends DefaultTask
 
     ClientLibsCompress()
     {
-        yuiCompressorJar = "${project.labkey.externalLibDir}/build/yuicompressor-${project.yuiCompressorVersion}.jar"
     }
 
     @InputFiles
@@ -46,19 +43,21 @@ class ClientLibsCompress extends DefaultTask
     {
         FileTree libXmlFiles = getLibXmlFiles()
         libXmlFiles.files.each() {
-            File file -> compressSingleFile(file.getAbsolutePath())
+            File file -> compressSingleFile(file)
         }
 
-        // this file is used by the ClientLibraryBuilder to determine if things are up to date
+        // this file is used to determine if things are up to date
         FileUtils.touch(upToDateFile)
     }
 
-
-
-    void compressSingleFile(String sourceFile)
+    void compressSingleFile(File xmlFile)
     {
-        File xmlFile = new File(sourceFile);
+        XmlImporter importer = parseXmlFile(xmlFile)
+        compressFiles(xmlFile, importer)
+    }
 
+    private XmlImporter parseXmlFile(File xmlFile)
+    {
         try
         {
             // XMLBeans would be cleaner, but I'm not sure if this
@@ -69,6 +68,7 @@ class ClientLibsCompress extends DefaultTask
             SAXParser parser = factory.newSAXParser();
             XmlImporter importer = new XmlImporter(xmlFile, xmlFile.getParentFile(), workingDir);
             parser.parse(xmlFile, importer);
+            return importer;
         }
         catch (Exception e)
         {
@@ -76,59 +76,227 @@ class ClientLibsCompress extends DefaultTask
         }
     }
 
-    private class XmlImporter extends DefaultHandler
+    private void compressFiles(File xmlFile, XmlImporter importer)
     {
-        private LinkedHashSet<File> _js = new LinkedHashSet<>();
-        private LinkedHashSet<File> _css = new LinkedHashSet<>();
-        private boolean _doCompile = true;
-        private boolean _withinScriptsTag = false;
-        private File _xmlFile;
-        private File _outputDir;
-        private File _sourceDir;
-
-        XmlImporter(File xml, File outputDir, File sourceDir)
+        if (importer.doCompile)
         {
-            _xmlFile = xml;
-            _sourceDir = sourceDir;
-            _outputDir = outputDir;
+            try
+            {
+                if (importer.getJavaScriptFiles().size() > 0)
+                    compileScripts(xmlFile, importer.getJavaScriptFiles(), "js");
+                if (importer.getCssFiles().size() > 0)
+                    compileScripts(xmlFile, importer.getCssFiles(), "css");
+            }
+            catch (Exception e)
+            {
+                throw new SAXException(e);
+            }
+        }
+        else
+        {
+            project.logger.info("No compile necessary");
+        }
+    }
+
+    void compileScripts(File xmlFile, Set<File> srcFiles, String extension) throws IOException, InterruptedException
+    {
+        File minFile = getOutputFile(xmlFile, "min", extension);
+
+        if (upToDate(xmlFile, minFile, srcFiles))
+        {
+            project.logger.info("files are up to date");
+            return;
         }
 
-        void endDocument() throws SAXException
+        project.logger.info("Concatenating " + extension + " files into single file: ");
+        File concatFile = getOutputFile(xmlFile, "combined", extension);
+        concatenateFiles(srcFiles, concatFile);
+
+        project.logger.info("Minifying " + extension + " files with YUICompressor: ");
+        minFile.delete();
+
+        minifyFile(concatFile, minFile);
+
+        concatFile.delete();
+
+        if (!LabKeyExtension.isDevMode(project))
         {
-            if (_doCompile)
+            project.logger.info("Compressing " + extension + " files");
+            project.ant.gzip(
+                    src: minFile,
+                    destfile: "${minFile.toString()}.gz"
+            )
+        }
+    }
+
+    private File getOutputFile(File xmlFile, String token, String ex)
+    {
+        return new File(xmlFile.getParentFile(), xmlFile.getName().replaceAll(LIB_XML_EXTENSION, "." + token + "." + ex));
+    }
+
+    private void concatenateFiles(Set<File> files, File output)
+    {
+        try
+        {
+            output.createNewFile();
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        PrintWriter saveAs = null
+        try
+        {
+            saveAs = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output), StandardCharsets.UTF_8)))
+
+            for (File f : files)
             {
+                BufferedReader readBuff = null
                 try
                 {
-                    if(_js.size() > 0)
-                        compileScripts(_js, "js");
-                    if(_css.size() > 0)
-                        compileScripts(_css, "css");
+                    readBuff = new BufferedReader(new InputStreamReader(new FileInputStream(f), StandardCharsets.UTF_8))
+
+                    String line = readBuff.readLine();
+
+                    while (line != null)
+                    {
+                        saveAs.println(line);
+                        line = readBuff.readLine();
+                    }
                 }
-                catch (Exception e)
+                finally
                 {
-                    throw new SAXException(e);
+                    IOUtils.closeQuietly(readBuff)
                 }
+
+            }
+        }
+        finally
+        {
+            IOUtils.closeQuietly(saveAs)
+        }
+    }
+
+
+    private List<String> getCompressorErrors(Set<File> srcFiles) throws IOException
+    {
+        List<String> allErrors = new ArrayList<>();
+        for (File srcFile : srcFiles)
+        {
+            File minFile = File.createTempFile(srcFile.getName(), null);
+            minFile.deleteOnExit();
+
+            List<String> errors = minifyFile(srcFile, minFile);
+
+            if (!errors.isEmpty())
+                allErrors.addAll(errors);
+        }
+        return allErrors;
+    }
+
+    // TODO convert to Gradle check with input and output file designations
+    private boolean upToDate(File xmlFile, File destFile, Set<File> srcFiles)
+    {
+        long ts = 0;
+        long lastModified;
+
+        if (destFile.exists())
+            ts = destFile.lastModified();
+        else
+            return false;
+
+        //test the xml file itself
+        lastModified = xmlFile.lastModified();
+        project.logger.debug("${xmlFile}  is ${lastModified < ts ? "older" : "newer"}  than ${destFile.getPath()}");
+        if (lastModified > ts)
+            return false;
+
+        //then test resources
+        for (File srcFile : srcFiles)
+        {
+            if (srcFile.exists())
+            {
+                lastModified = srcFile.lastModified();
+                project.logger.debug("${srcFile.getPath()}  is ${ (lastModified < ts ? "older" : "newer") }  than ${destFile.getPath()}");
+                if (lastModified > ts)
+                    return false;
             }
             else
             {
-                project.logger.info("No compile necessary");
+                project.logger.error("File does not exist: " + srcFile.getPath());
             }
+        }
+        return true;
+    }
+
+    private void minifyFile(File srcFile, File destFile) throws IOException
+    {
+        if (srcFile.getName().endsWith("js"))
+        {
+            srcFile.withReader { reader ->
+                JavaScriptCompressor compressor = new JavaScriptCompressor(reader, null)
+
+                destFile.withWriter { writer ->
+                    compressor.compress(writer, null, -1, true, false, false, false)
+                }
+            }
+        }
+        else
+        {
+            srcFile.withReader { reader ->
+                CssCompressor compressor = new CssCompressor(reader)
+                destFile.withWriter { writer ->
+                    compressor.compress(writer, -1)
+                }
+            }
+        }
+    }
+
+    private class XmlImporter extends DefaultHandler
+    {
+        private boolean withinScriptsTag = false;
+        private File xmlFile;
+        private File sourceDir;
+        private LinkedHashSet<File> javaScriptFiles = new LinkedHashSet<>()
+        private LinkedHashSet<File> cssFiles = new LinkedHashSet<>()
+        private boolean doCompile = true
+
+        XmlImporter(File xml, File outputDir, File sourceDir)
+        {
+            xmlFile = xml;
+            this.sourceDir = sourceDir;
+        }
+
+        LinkedHashSet<File> getJavaScriptFiles()
+        {
+            return javaScriptFiles
+        }
+
+        LinkedHashSet<File> getCssFiles()
+        {
+            return cssFiles
+        }
+
+        boolean getDoCompile()
+        {
+            return doCompile
         }
 
         void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException
         {
             if ("library".equals(localName))
             {
-                if(attributes.getValue("compileInProductionMode") != null)
+                if (attributes.getValue("compileInProductionMode") != null)
                 {
-                    _doCompile = Boolean.parseBoolean(attributes.getValue("compileInProductionMode"));
+                    doCompile = Boolean.parseBoolean(attributes.getValue("compileInProductionMode"));
                 }
-                _withinScriptsTag = true;
+                withinScriptsTag = true;
             }
-            if (_withinScriptsTag && "script".equals(localName))
+            if (withinScriptsTag && "script".equals(localName))
             {
                 String path = attributes.getValue("path");
-                File scriptFile = new File(_sourceDir, path);
+                File scriptFile = new File(sourceDir, path);
                 if (!scriptFile.exists())
                 {
                     if (isExternalScript(path))
@@ -137,7 +305,7 @@ class ClientLibsCompress extends DefaultTask
                     }
                     else
                     {
-                        throw new RuntimeException("ERROR: Unable to find script file: " + scriptFile + " from library: " + _xmlFile);
+                        throw new RuntimeException("ERROR: Unable to find script file: " + scriptFile + " from library: " + xmlFile);
                     }
                 }
                 else
@@ -157,9 +325,9 @@ class ClientLibsCompress extends DefaultTask
                     }
 
                     if (scriptFile.getName().endsWith(".js"))
-                        _js.add(scriptFile);
+                        javaScriptFiles.add(scriptFile);
                     else if (scriptFile.getName().endsWith(".css"))
-                        _css.add(scriptFile);
+                        cssFiles.add(scriptFile);
                     else
                         project.logger.info("Unknown file extension, ignoring: " + scriptFile.getName());
                 }
@@ -169,193 +337,12 @@ class ClientLibsCompress extends DefaultTask
         void endElement(String uri, String localName, String qName) throws SAXException
         {
             if ("library".equals(localName))
-                _withinScriptsTag = false;
-        }
-
-        private File getOutputFile(String token, String ex)
-        {
-            return new File(_outputDir, _xmlFile.getName().replaceAll(LIB_XML_EXTENSION, "." + token + "." + ex));
-        }
-
-        private void concatenateFiles(Set<File> files, File output)
-        {
-            try
-            {
-                output.createNewFile();
-            }
-            catch (IOException e)
-            {
-                throw new RuntimeException(e);
-            }
-
-            PrintWriter saveAs = null
-            try
-            {
-                saveAs = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output), StandardCharsets.UTF_8)))
-
-                for (File f : files)
-                {
-                    BufferedReader readBuff = null
-                    try
-                    {
-                        readBuff = new BufferedReader(new InputStreamReader(new FileInputStream(f), StandardCharsets.UTF_8))
-
-                        String line = readBuff.readLine();
-
-                        while (line != null)
-                        {
-                            saveAs.println(line);
-                            line = readBuff.readLine();
-                        }
-                    }
-                    finally
-                    {
-                        IOUtils.closeQuietly(readBuff)
-                    }
-
-                }
-            }
-            finally
-            {
-                IOUtils.closeQuietly(saveAs)
-            }
+                withinScriptsTag = false;
         }
 
         private boolean isExternalScript(String path)
         {
             return path != null && (path.contains("http://") || path.contains("https://"));
         }
-
-        private void compileScripts(Set<File> srcFiles, String extension) throws IOException, InterruptedException
-        {
-            // Starting with 16.1, build.xml passes in the yuicompressor.jar location as a parameter.
-            if (null == yuiCompressorJar)
-                throw new RuntimeException("Must specify yuicompressor jar location via \"yuicompressorjar\" parameter");
-
-            File minFile = getOutputFile("min", extension);
-
-            if(upToDate(minFile, srcFiles))
-            {
-                project.logger.info("files are up to date");
-                return;
-            }
-
-            project.logger.info("Concatenating " + extension + " files into single file: ");
-            File concatFile = getOutputFile("combined", extension);
-            concatenateFiles(srcFiles, concatFile);
-
-            project.logger.info("Minifying " + extension + " files with YUICompressor: ");
-            minFile.delete();
-
-            List<String> errors = minifyFile(concatFile, minFile);
-
-            concatFile.delete();
-
-            if (!errors.isEmpty() || !minFile.exists())
-            {
-                project.logger.error("YUICompressor errors");
-                project.logger.error(String.join("\n", getCompressorErrors(srcFiles)));
-                throw new RuntimeException("ERROR: YUICompressor did not run properly for '" + extension + "' files in " +
-                        _xmlFile.getName() + ". See log for more details.");
-            }
-            if (!LabKeyExtension.isDevMode(project))
-            {
-                project.logger.info("Compressing " + extension + " files");
-                project.ant.gzip(
-                        src: minFile,
-                        destfile: "${minFile.toString()}.gz"
-                )
-            }
-        }
-
-        private List<String> getCompressorErrors(Set<File> srcFiles) throws IOException
-        {
-            List<String> allErrors = new ArrayList<>();
-            for (File srcFile : srcFiles)
-            {
-                File minFile = File.createTempFile(srcFile.getName(), null);
-                minFile.deleteOnExit();
-
-                List<String> errors = minifyFile(srcFile, minFile);
-
-                if (!errors.isEmpty())
-                    allErrors.addAll(errors);
-            }
-            return allErrors;
-        }
-
-        // TODO convert to Gradle check with input and output file designations
-        private boolean upToDate(File destFile, Set<File> srcFiles)
-        {
-            long ts = 0;
-            long lastModified;
-
-            if (destFile.exists())
-                ts = destFile.lastModified();
-            else
-                return false;
-
-            //test the xml file itself
-            lastModified = _xmlFile.lastModified();
-            project.logger.debug("${_xmlFile}  is ${lastModified < ts ? "older" : "newer"}  than ${destFile.getPath()}");
-            if (lastModified > ts)
-                return false;
-
-            //then test resources
-            for (File srcFile : srcFiles)
-            {
-                if (srcFile.exists())
-                {
-                    lastModified = srcFile.lastModified();
-                    project.logger.debug("${srcFile.getPath()}  is ${ (lastModified < ts ? "older" : "newer") }  than ${destFile.getPath()}");
-                    if (lastModified > ts)
-                        return false;
-                }
-                else
-                {
-                    project.logger.error("File does not exist: " + srcFile.getPath());
-                }
-            }
-            return true;
-        }
     }
-
-    private List<String> minifyFile(File srcFile, File destFile) throws IOException
-    {
-        // TODO convert this to a javaexec action.
-        File j = new File(yuiCompressorJar);
-        if (!j.exists())
-            throw new RuntimeException("YUI JAR not found at expected location: " + j.getAbsolutePath());
-
-        List<String> params = new LinkedList<>();
-        params.add("java");
-        params.add("-Dfile.encoding=UTF-8");
-        params.add("-jar");
-        params.add(j.getAbsolutePath());
-        params.add("-o");
-        params.add(destFile.getPath());
-        params.add(srcFile.getPath());
-
-        ProcessBuilder pb = new ProcessBuilder(params);
-        Process p = pb.start();
-
-        List<String> errors = new ArrayList<>();
-        BufferedReader reader = null
-        try
-        {
-            reader = new BufferedReader(new InputStreamReader(p.getErrorStream(), "US-ASCII"))
-
-            String line;
-            while ((line = reader.readLine()) != null)
-            {
-                errors.add(line);
-            }
-            return errors;
-        }
-        finally
-        {
-            IOUtils.closeQuietly(reader)
-        }
-    }
-
 }
