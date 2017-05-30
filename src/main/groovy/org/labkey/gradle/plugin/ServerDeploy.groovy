87fef69099fa644e78f8ecad75e2b96168f3d88f
Susan Hert <susanh@labkey.com>
2017-05-29 19:33:52 -0700

    Issue 30461: special (hacky) logic for keeping previous names of jar files deployed to tomcat directory

--- src/main/groovy/org/labkey/gradle/plugin/ServerDeploy.groovy
+++ src/main/groovy/org/labkey/gradle/plugin/ServerDeploy.groovy
@@ -106,10 +106,23 @@ class ServerDeploy implements Plugin<Project>
                         // Put unversioned files into the tomcatLibDir.  These files are meant to be copied into
                         // the tomcat/lib directory when deploying a build or a distribution.  When version numbers change,
                         // you will end up with multiple versions of these jar files on the classpath, which will often
-                        // result in problems of compatibility.  We may employ CATALINA_BASE in order to separate our
-                        // libraries from the ones that come with the tomcat distribution. This will require updating our
-                        // instructions for installation by clients.
-                        regexpmapper(from: "^(.*?)(-\\d+(\\.\\d+)*(-\\.*)?(-SNAPSHOT)?)?\\.jar", to: "\\1.jar")
+                        // result in problems of compatibility.  Additionally, we want to maintain the (incorrect) versions
+                        // of the files that have been used with the Ant build process.
+                        //
+                        // We may employ CATALINA_BASE in order to separate our libraries from the ones that come with
+                        // the tomcat distribution. This will require updating our instructions for installation by clients
+                        // but would allow us to use artifacts with more self-documenting names.
+                        chainedmapper()
+                                {
+                                    flattenmapper()
+                                    // get rid of the version numbers on the jar files
+                                    regexpmapper(from: "^(.*?)(-\\d+(\\.\\d+)*(-\\.*)?(-SNAPSHOT)?)?\\.jar", to: "\\1.jar")
+                                    filtermapper()
+                                            {
+                                                replacestring(from: "mysql-connector-java", to: "mysql") // the Ant build used mysql.jar
+                                                replacestring(from: "javax.mail", to: "mail") // the Ant build used mail.jar
+                                            }
+                                }
                     }
         })
 
