6b0c9f7c0e811d6bbc66b79c48b6e6ce536b412e
Susan Hert <susanh@labkey.com>
2018-04-03 09:06:21 -0700

    Issue 32153: change order of extension creation to get the right properties available at the right time

--- src/main/groovy/org/labkey/gradle/plugin/TeamCity.groovy
+++ src/main/groovy/org/labkey/gradle/plugin/TeamCity.groovy
@@ -49,9 +49,11 @@ class TeamCity extends Tomcat
     @Override
     void apply(Project project)
     {
+        extension = project.extensions.create("teamCity", TeamCityExtension, project)
+        // we apply the parent plugin after creating the teamCity extension because we need some of the properties
+        // from TeamCity's configuration when creating the UITestExtension on TeamCity
         super.apply(project)
         project.tomcat.assertionFlag = "-ea"
-        extension = project.extensions.create("teamCity", TeamCityExtension, project)
         if (project.file("${project.tomcatDir}/localhost.truststore").exists())
         {
             project.tomcat.trustStore = "-Djavax.net.ssl.trustStore=${project.tomcatDir}/localhost.truststore"
